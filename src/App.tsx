02bd3488d9d8599b632d6b6ab35c816613d0e1e1
Jonathan Tuzman <jtuzman@sunriselabs.com>
2020-02-08 13:45:46 -0500

    Add controlled text area

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [text, setText] = useState('# a heading to start');
+  
+  const onChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value)
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Markdown</h1>
+      <textarea value={ text } onChange={ onChange }/>
     </div>
   );
-}
+};
 
 export default App;
